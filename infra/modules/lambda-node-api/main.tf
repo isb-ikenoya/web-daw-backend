51a3b3a55ca06fdc2e3ca972fb6646801fe9564a
yuta_ikenoya <yuta_ikenoya@isb.co.jp>
2026-03-12 11:25:27 +0900

    plan差分テスト3

--- infra/modules/lambda-node-api/main.tf
+++ infra/modules/lambda-node-api/main.tf
@@ -7,7 +7,7 @@ resource "aws_lambda_layer_version" "demo_layer" {
   filename = local.layer_zip_path
   # Plan時のエラーを防ぐため、ファイルがあればハッシュを取り、なければ一旦 null にする
   # オリジナルのpackage-lock.jsonで判定
-  # テストコメント2
+  # テストコメント3
   source_code_hash    = var.package_lock_hash != "" ? base64encode(var.package_lock_hash) : null
   layer_name          = "layer-demo-ikenoya"
   compatible_runtimes = ["nodejs22.x"]
