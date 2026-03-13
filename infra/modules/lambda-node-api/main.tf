69625ffb3f4df87f1f7c0d0f75fafa48d1576c90
yuta_ikenoya <yuta_ikenoya@isb.co.jp>
2026-03-13 10:37:43 +0900

    【Lambda】レイヤー用のS3オブジェクトを明示的に作成したリソースに紐付け

--- infra/modules/lambda-node-api/main.tf
+++ infra/modules/lambda-node-api/main.tf
@@ -20,7 +20,7 @@ resource "aws_s3_bucket" "layer_upload" {
 
 resource "aws_s3_object" "layer_upload" {
   count  = var.layer_upload_s3 != null ? 1 : 0
-  bucket = var.layer_upload_s3.bucket_name
+  bucket = aws_s3_bucket.layer_upload[0].bucket
   key    = var.layer_upload_s3.key
 
   # アーカイブしたzipファイルを指定
